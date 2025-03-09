5bc7e5394f2bc620e4ff4de05b0f8b7b0b16756b
mjilani98 <mjilani@emich.edu>
2025-03-09 12:27:22 -0400

    Create MainActivtiy.java file

--- app/src/main/java/com/example/hw3q2/MainActivity.java
+++ app/src/main/java/com/example/hw3q2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.hw3q2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,26 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static TuitionCalculator tuitionCalculator;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
+        //create a tuition calculator object
+        tuitionCalculator = new TuitionCalculator();
+
+        //display the main screen
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    }
+
+    //event handler that will start the second scree
+    public void next(View v)
+    {
+        //create second activity
+        Intent secondActivity = new Intent(this,SecondActivity.class);
+
+        //display the second screen
+        startActivity(secondActivity);
     }
 }
